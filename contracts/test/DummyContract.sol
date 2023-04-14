57fc4c95150ef5a70962d66a35f0d9d4903d4739
abhishekvispute <abhivispute33@gmail.com>
2023-04-15 00:54:53 +0530

    test: execute happy case

---
+++ contracts/test/DummyContract.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Unlicense
+
+pragma solidity ^0.8.19;
+
+contract DummyContract {
+    
+    uint256 public x;
+    function setX(uint256 _x) public payable {
+        x = _x;
+    }
+    
+    function sayHello() public pure returns (string memory) {
+        return "Hello";
+    }
+
+    function sheSaidNo() public pure {
+        revert("sorry, I have a boyfriend");
+    }
+   
+}
