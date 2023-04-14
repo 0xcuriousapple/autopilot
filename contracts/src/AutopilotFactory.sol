594274347c81f8f2e91acfef19b8f80b54ef7307
abhishekvispute <abhivispute33@gmail.com>
2023-04-14 17:57:13 +0530

    fix: update factory interface

--- contracts/src/AutopilotFactory.sol
+++ contracts/src/AutopilotFactory.sol
@@ -5,7 +5,7 @@ import "@openzeppelin/contracts/utils/Create2.sol";
 
 import {Autopilot, IEntryPoint} from "./Autopilot.sol";
 
-contract AutopilotAccountFactory {
+contract AutopilotFactory {
 
     IEntryPoint public immutable entryPoint;
     constructor(IEntryPoint _entryPoint) { 
@@ -13,18 +13,18 @@ contract AutopilotAccountFactory {
     }
 
    
-    function createAccount(address owner,uint256 salt) public returns (Autopilot ret) {
+    function createAccount(address owner,bytes32 salt) public returns (Autopilot ret) {
         address addr = getAddress(owner, salt);
         uint codeSize = addr.code.length;
         if (codeSize > 0) {
             return Autopilot(payable(addr));
         }
-        ret = Autopilot(new Autopilot{salt : bytes32(salt)}(entryPoint, owner));
+        ret = Autopilot(new Autopilot{salt : salt}(entryPoint, owner));
     }
 
     
-    function getAddress(address owner,uint256 salt) public view returns (address) {
-        return Create2.computeAddress(bytes32(salt), keccak256(abi.encodePacked(
+    function getAddress(address owner,bytes32 salt) public view returns (address) {
+        return Create2.computeAddress(salt, keccak256(abi.encodePacked(
                 type(Autopilot).creationCode,
                 abi.encode(
                     address(entryPoint),
